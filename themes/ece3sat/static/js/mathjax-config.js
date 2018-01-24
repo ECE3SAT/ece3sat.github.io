2aa0d42de9adf4021882142bc2c85227b8617d18
Julien Bréhin <julbrehin@gmail.com>
2018-01-24 20:55:51 +0100

    Added equations rendering with MathJax
    + Added MathJax static content in website template
    + Added mathjax_config.js script to configure MathJax
    + Added script imports in <head></head> tags for the theme's page template
    (layouts/_default/baseof.html)

---
+++ themes/ece3sat/static/js/mathjax-config.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\[','\]']],
+    processEscapes: true,
+    processEnvironments: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+    TeX: { equationNumbers: { autoNumber: "AMS" },
+           extensions: ["AMSmath.js", "AMSsymbols.js"] }
+     }
+};
